e0a3678f0c659480e9796c10c18bdc98f868c5fa
Diana <slorrrr@gmail.com>
2022-01-11 00:27:55 -0500

    create favourites link and page

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 app.use(cookieSession({
   name: 'session',
-  keys: ['midterm']
+  keys: ['key1']
 }));
 
 // app.use(
@@ -47,10 +47,9 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 const registerRoutes = require("./routes/register");
-const loginRoutes = require("./routes/login");
-const filterRoutes = require("./routes/filters");
-const productRoutes = require("./routes/products");
-const listingRoutes = require("./routes/createListing");
+const loginRoutes = require("./routes/login")
+const filterRoutes = require("./routes/filters")
+const productRoutes = require("./routes/products")
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -60,9 +59,7 @@ app.use("/register", registerRoutes);
 app.use("/login", loginRoutes);
 app.use("/filters", filterRoutes);
 app.use("/products", productRoutes);
-app.use("/createlisting", listingRoutes(db));
 // Note: mount other resources here, using the same pattern above
-
 
 // Home page
 // Warning: avoid creating more routes in this file!
@@ -70,54 +67,79 @@ app.use("/createlisting", listingRoutes(db));
 
 app.get("/", (req, res) => {
   return db.query(`SELECT * FROM products ORDER BY id`)
-    .then((response) => {
-      console.log("response:", response.rows);
-      const templateVars = {
-        product: response.rows
-      };
-      console.log("template:", templateVars);
-      res.render("index", templateVars);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  .then((response) => {
+    console.log("response:", response.rows)
+    const templateVars = {
+      product: response.rows
+    }
+    console.log("template:", templateVars)
+    res.render("index", templateVars);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
 });
 
 app.get("/favourites", (req, res) => {
+  const id = req.session.userID
+  console.log(id)
+  console.log("123")
+
   const queryString = `
-    SELECT * FROM products
+    SELECT products.*, favourites.* FROM products
     JOIN favourites ON product_id = products.id
-    WHERE products.id = $1;
-  `;
-  db.query(queryString, [1])
+    WHERE favourites.user_id = $1;
+  `
+  db.query(queryString, [id])
     .then(response => {
-      console.log("res:", response.rows);
+      console.log("res:", response.rows)
       const templateVars = {
         product: response.rows
-      };
-      console.log("template:", templateVars);
+      }
+      console.log("template:", templateVars)
       res.render("favourites", templateVars);
     })
     .catch((err) => {
-      console.log(err.message);
-    });
+    console.log(err.message);
+  });
 });
+
+app.post("/favourites", (req, res) => {
+  const product_id = req.body.product_id;
+  console.log(product_id);
+  const user_id = req.session.userID;
+
+  db.query(`
+    INSERT INTO favourites (user_id, product_id) VALUES ($1, $2)
+  `, [user_id, product_id])
+  .then((response) => {
+    console.log(response)
+    res.send("success!~")
+  })
+  .catch((err) => {
+    console.log(err.message);
+    res.send("failure")
+  });
+
+})
+
+
 
 app.get("/messages", (req, res) => {
 
   db.query(`SELECT * FROM messages JOIN messagethreads ON messagethreads.id = message_thread_id JOIN products ON products.id = product_id JOIN users ON users.id = seller_id WHERE product_id = $1`, [4])
-    .then((response) => {
-      console.log("response:", response.rows[0]);
-      const templateVars = {
-        product: response.rows[0]
-      };
-      console.log("template:", templateVars);
-      res.render("messages", templateVars);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
+  .then((response) => {
+    console.log("response:", response.rows[0])
+    const templateVars = {
+      product: response.rows[0]
+    }
+    console.log("template:", templateVars)
+    res.render("messages", templateVars);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
+})
 
 // app.get("/:id/messages", (req, res) => {
 //   const id = req.params.id;
