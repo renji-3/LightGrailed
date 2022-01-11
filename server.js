d827d9e69bb3792177c0515eac25666fddbb8f36
Diana <slorrrr@gmail.com>
2022-01-11 13:15:00 -0500

    create delete route to unfavourite and logout

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ const loginRoutes = require("./routes/login")
 const filterRoutes = require("./routes/filters")
 const productRoutes = require("./routes/products")
 const listingRoutes = require("./routes/createListing");
+const favouritesRoutes = require("./routes/favourites");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -61,6 +62,7 @@ app.use("/login", loginRoutes);
 app.use("/filters", filterRoutes);
 app.use("/products", productRoutes);
 app.use("/createlisting", listingRoutes(db));
+app.use("/favourites", favouritesRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -82,51 +84,6 @@ app.get("/", (req, res) => {
   })
 });
 
-app.get("/favourites", (req, res) => {
-  const id = req.session.userID
-  console.log(id)
-  console.log("123")
-
-  const queryString = `
-    SELECT products.*, favourites.* FROM products
-    JOIN favourites ON product_id = products.id
-    WHERE favourites.user_id = $1;
-  `
-  db.query(queryString, [id])
-    .then(response => {
-      console.log("res:", response.rows)
-      const templateVars = {
-        product: response.rows
-      }
-      console.log("template:", templateVars)
-      res.render("favourites", templateVars);
-    })
-    .catch((err) => {
-    console.log(err.message);
-  });
-});
-
-app.post("/favourites", (req, res) => {
-  const product_id = req.body.product_id;
-  console.log(product_id);
-  const user_id = req.session.userID;
-
-  db.query(`
-    INSERT INTO favourites (user_id, product_id) VALUES ($1, $2)
-  `, [user_id, product_id])
-  .then((response) => {
-    console.log(response)
-    res.send("success!~")
-  })
-  .catch((err) => {
-    console.log(err.message);
-    res.send("failure")
-  });
-
-})
-
-
-
 app.get("/messages", (req, res) => {
 
   db.query(`SELECT * FROM messages JOIN messagethreads ON messagethreads.id = message_thread_id JOIN products ON products.id = product_id JOIN users ON users.id = seller_id WHERE product_id = $1`, [4])
@@ -143,23 +100,10 @@ app.get("/messages", (req, res) => {
   })
 })
 
-// app.get("/:id/messages", (req, res) => {
-//   const id = req.params.id;
-
-//   db.query(`SELECT * FROM messages JOIN messagethreads ON messagethreads.id = message_thread_id JOIN products ON products.id = product_id JOIN users ON users.id = seller_id WHERE product_id = $1`, [id])
-//   .then((response) => {
-//     console.log("response:", response.rows[0])
-//     const templateVars = {
-//       product: response.rows[0]
-//     }
-//     console.log("template:", templateVars)
-//     res.render("thread", templateVars);
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   })
-// })
-
+app.post('/logout', (req, res) => {
+  req.session = null;
+  res.redirect("/")
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
