4289f4be002e5cf4be31cf5889b87978cb6911f5
Diana <slorrrr@gmail.com>
2022-01-10 20:15:11 -0500

    linked homepage to database

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const cookieSession = require('cookie-session');
 const { Pool } = require("pg");
 const dbParams = require("../lib/db.js");
 const db = new Pool(dbParams);
@@ -9,15 +10,47 @@ router.get("/", (req, res) => {
   res.render("login")
 });
 
+// const getUsername = function(username) {
+//   return db
+//   .query(`SELECT * FROM users WHERE username = $1`, [username])
+//   .then((response) => response.rows[0])
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+// }
+
+// const login = function(username, password) {
+//   return db.getUsername(username)
+//   .then(user => {
+//     if (bcrypt.compareSync(password, user.password)) {
+//       return user;
+//     }
+//     return null;
+//   });
+// }
+
 router.post("/", (req, res) => {
+  console.log("cookie:", req.session)
+  console.log("body:", req.body)
   const username = req.body.username;
   const password = req.body.password;
+
+//   login(email, password)
+//     .then(user => {
+//       if (!user) {
+//         res.send({error: "error"});
+//         return;
+//       }
+//       req.session.userId = user.userId
+//       res.redirect("/")
+//     })
 
   return db
   .query(`SELECT * FROM users WHERE username = $1`, [username])
   .then((response) => {
     console.log("before res", response)
     if (bcrypt.compareSync(password, response.rows[0].password)) {
+    console.log(req.session)
     console.log("res", response)
     }
     res.redirect("/")
